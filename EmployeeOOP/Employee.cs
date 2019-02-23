a4bb9c4f0d83ff557bfe8e8d749d723056e7a50d
lutfiqaraman <lutfiqaraman@gmail.com>
2019-02-23 21:16:09 +0200

    Done

---
+++ EmployeeOOP/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP
+{
+    public class Employee
+    {
+        private readonly string FullName;
+        private readonly int    Hours;
+        private readonly double Rate;
+
+        public Employee(string fullname, int hours, double rate)
+        {
+            FullName = fullname;
+            Hours    = hours;
+            Rate     = rate;
+        }
+
+        public double CalcualteSalary ()
+        {
+            double TotalSalary = (Hours * Rate) * 30;
+            return TotalSalary;
+        }
+
+        public void PrintSlip()
+        {
+            Console.WriteLine("The Slip");
+            Console.WriteLine("=========");
+            Console.WriteLine("Name: {0}", FullName);
+            Console.Write("Salary: {0} USD", CalcualteSalary());
+        }
+    }
+}
---
+++ EmployeeOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "John Dark";
+            int Hours = 10;
+            double Rate = 15.75;
+
+            Employee Emp1 = new Employee(Name, Hours, Rate);
+            Emp1.PrintSlip();
+
+            Console.ReadKey();
+        }
+    }
+}
